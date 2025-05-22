3a0f1d9d41b73d1c40cb178adefc1cddd51b855b
GregoryArod <Greyyy84@gmail.com>
2025-05-22 01:06:42 -0400

    Add files via upload

---
+++ tarea 1/trabajo code/1.1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <studio.h>
+
+/* Programa 1.1
+el siguiente es el primer programa en el lenguaje c. */
+
+void main (void)
+{
+    printf("Mi primer programa en C" ) ;}
---
+++ tarea 1/trabajo code/1.2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int main(void)
+{
+    int A, B, C, D;
+    printf("Ingrese cuatro datos de tipo entero: ");
+    scanf("%d %d %d %d", &A, &B, &C, &D);
+    printf("\n%d %d %d %d\n", D, C, B, A);
+}

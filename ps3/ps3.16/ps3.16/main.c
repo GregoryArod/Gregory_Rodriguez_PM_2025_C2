2fbbaf8b58bfac4235d0ff84fe5393da7d661e4a
GregoryArod <Greyyy84@gmail.com>
2025-06-05 01:36:44 -0400

    Add files via upload

---
+++ ps3/ps3.16/ps3.16/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int main() {
+    int N, i, j;
+
+    printf("Ingresa un numero entero positivo: ");
+    scanf("%d", &N);
+
+    // Parte ascendente
+    for (i = 1; i <= N; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    // Parte descendente
+    for (i = N - 1; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
